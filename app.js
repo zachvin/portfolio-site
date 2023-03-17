3b5345aedcf93ab0685c8c1f0e385a748f6644ef
Zach Vincent <zvincent@nd.edu>
2023-03-17 00:23:15 -0400

    added glowing text and dot background

---
+++ app.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((el) => observer.observe(el));
